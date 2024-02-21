9a5e95da1fac9fb2751bb48d3f9c56391cf7ac82
F0otsh0T <jychen1@hotmail.com>
2024-02-21 10:20:51 -0800

    [F0otsh0T 20240221] standardize on vault-ent-network

--- docker-keycloak/terraform/99.outputs.tf
+++ docker-keycloak/terraform/99.outputs.tf
@@ -33,15 +33,15 @@ output "container_image_keycloak_name" {
 ################################################
 
 output "network_name" {
-  value = docker_network.keycloak-network.name
+  value = docker_network.vault-ent-network.name
 }
 
 output "network_id" {
-  value = docker_network.keycloak-network.id
+  value = docker_network.vault-ent-network.id
 }
 
 output "network_ipam" {
-  value = docker_network.keycloak-network.ipam_config
+  value = docker_network.vault-ent-network.ipam_config
 }
 
 ################################################
